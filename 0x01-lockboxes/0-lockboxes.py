52dfa82d14969d32d3cba382375d90bacab87b1b
kok <kok@ubuntu.ubuntu-domain>
2024-07-04 13:38:24 -0700

    updated first line

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -3,24 +3,13 @@
 
 
 def canUnlockAll(boxes):
-    # Number of boxes
-    n = len(boxes)
-    
-    # A set to keep track of visited boxes
-    visited = set()
-    
-    # A queue to keep track of keys to check
-    queue = [0]
-    
-    # Perform BFS
-    while queue:
-        current = queue.pop(0)
-        
-        if current not in visited:
-            visited.add(current)
-            for key in boxes[current]:
-                if key < n and key not in visited:
-                    queue.append(key)
-    
-    # Check if all boxes are visited
-    return len(visited) == n
+    '''Method that determines if all boxes can be opened'''
+    for key in range(1, len(boxes)):
+        flag = False
+        for box in range(len(boxes)):
+            if key in boxes[box] and box != key:
+                flag = True
+                break
+        if not flag:
+            return False
+    return True
