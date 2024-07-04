a8446431028b7aaf77330794f697be0fb262906b
kok <kok@ubuntu.ubuntu-domain>
2024-07-04 13:18:26 -0700

    solution

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -3,13 +3,17 @@
 
 
 def canUnlockAll(boxes):
-    '''Method that determines if all boxes can be opened'''
-    for key in range(1, len(boxes)):
-        flag = False
-        for box in range(len(boxes)):
-            if key in boxes[box] and box != key:
-                flag = True
-                break
-        if not flag:
-            return False
-    return True
+    '''func to unlock boxes'''
+    n = len(boxes)
+    unlocked = [False] * n  # Track unlocked boxes
+    unlocked[0] = True  # The first box is always unlocked
+    queue = [0]  # Start with the first box
+
+    while queue:
+        box_index = queue.pop(0)
+        for key in boxes[box_index]:
+            if key < n and not unlocked[key]:
+                unlocked[key] = True
+                queue.append(key)
+
+    return all(unlocked)
