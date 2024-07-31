93a8af91c6ac952c3dd0e4d7a381c8f7ada50dad
kok <kok@ubuntu.ubuntu-domain>
2024-07-31 16:12:35 +0300

    added project tasks

---
+++ 0x05-nqueens/0-nqueens.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python3
+'''N queens'''
+import sys
+
+
+def is_safe(board, row, col):
+    """Check if it's safe to place a queen at board[row][col]."""
+    for i in range(col):
+        if board[row][i] == 1:
+            return False
+
+    for i, j in zip(range(row, -1, -1), range(col, -1, -1)):
+        if board[i][j] == 1:
+            return False
+
+    for i, j in zip(range(row, len(board)), range(col, -1, -1)):
+        if board[i][j] == 1:
+            return False
+
+    return True
+
+
+def solve_nqueens_util(board, col, solutions):
+    """Utilize backtracking to solve the N Queens problem."""
+    if col >= len(board):
+        solutions.append([[i, board[i].index(1)] for i in range(len(board))])
+        return True
+
+    res = False
+    for i in range(len(board)):
+        if is_safe(board, i, col):
+            board[i][col] = 1
+            res = solve_nqueens_util(board, col + 1, solutions) or res
+            board[i][col] = 0  # backtrack
+
+    return res
+
+
+def solve_nqueens(N):
+    """Solve the N Queens problem and print the solutions."""
+    board = [[0 for _ in range(N)] for _ in range(N)]
+    solutions = []
+    solve_nqueens_util(board, 0, solutions)
+    for solution in solutions:
+        print(solution)
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: nqueens N")
+        sys.exit(1)
+
+    try:
+        N = int(sys.argv[1])
+    except ValueError:
+        print("N must be a number")
+        sys.exit(1)
+
+    if N < 4:
+        print("N must be at least 4")
+        sys.exit(1)
+
+    solve_nqueens(N)
