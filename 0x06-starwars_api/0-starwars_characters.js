10feb5d36203bb695b6fb20d136cafa23712c63f
kok <jamesss075@gmail.com>
2024-08-09 08:45:51 +0300

    added project task

---
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -0,0 +1,30 @@
+#!/usr/bin/node
+
+const request = require("request");
+const movieId = process.argv[2];
+const url = `https://swapi-api.hbtn.io/api/films/${movieId}/`;
+
+request(url, (error, response, body) => {
+  if (error) {
+    console.error(error);
+  } else {
+    const characters = JSON.parse(body).characters;
+    const promises = characters.map((character) => {
+      return new Promise((resolve, reject) => {
+        request(character, (error, response, body) => {
+          if (error) {
+            reject(error);
+          } else {
+            resolve(JSON.parse(body).name);
+          }
+        });
+      });
+    });
+
+    Promise.all(promises)
+      .then((names) => {
+        names.forEach((name) => console.log(name));
+      })
+      .catch((error) => console.error(error));
+  }
+});
