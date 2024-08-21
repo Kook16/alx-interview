fe41be452aa29fb6a95d5e0b455c77193675746b
kok <kookcalvinayen@gmail.com>
2024-08-21 20:38:33 +0300

    Implement function to calculate the fewest number of coins needed to meet a given amount

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+'''makeChanges function'''
+
+
+def makeChange(coins, total):
+    '''
+    fewest number of coins needed to meet total
+    If total is 0 or less, return 0
+    If total cannot be met by any number of coins you have, return -1
+    '''
+    if total <= 0:
+        return 0
+
+    dp = [float('inf')] * (total + 1)
+    dp[0] = 0
+
+    for coin in coins:
+        for i in range(coin, total + 1):
+            dp[i] = min(dp[i], dp[i - coin] + 1)
+
+    return dp[total] if dp[total] != float('inf') else -1
